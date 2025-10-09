6a839eb7edf3705ff97b02695cc1692b9d6f4cf1
appsecandrewlee <appsec.andrewlee@gmail.com>
2025-10-09 15:20:17 +1100

    adding backend template

---
+++ andrew-PMS/src/main/java/com/example/andrew_PMS/AndrewPmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.andrew_PMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrewPmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrewPmsApplication.class, args);
+	}
+
+}
