39258665702d9967cb2bf9b9c6ad078d9744bd94
appsecandrewlee <appsec.andrewlee@gmail.com>
2025-10-10 15:15:52 +1100

    system version default upgrade

---
+++ andrew-improve-system/src/main/java/com/example/andrew_improve_system/AndrewImproveSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.andrew_improve_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndrewImproveSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndrewImproveSystemApplication.class, args);
+	}
+
+}
