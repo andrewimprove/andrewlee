de5aaa2da292654f3e5eda493c8051bff5ee2f01
appsecandrewlee <appsec.andrewlee@gmail.com>
2025-10-11 17:25:12 +1100

    generated setters and getters, User entity should suffice for now

--- andrew-improve-system/src/main/java/com/example/andrew_improve_system/controllers/UserController.java
+++ andrew-improve-system/src/main/java/com/example/andrew_improve_system/controllers/UserController.java
@@ -1,8 +1,24 @@
 package com.example.andrew_improve_system.controllers;
 
+import com.example.andrew_improve_system.entities.User;
 import com.example.andrew_improve_system.repositories.UserRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
 
+    private User user;
+
+
+    //Create User
+
+
+    //Read user
+
+    //Update User
+
+    //Delete User
 
 }
--- andrew-improve-system/src/main/java/com/example/andrew_improve_system/entities/User.java
+++ andrew-improve-system/src/main/java/com/example/andrew_improve_system/entities/User.java
@@ -8,32 +8,90 @@ import jakarta.validation.constraints.Size;
 
 @Entity
 public class User{
+
     @Id
     private String id;
-
     @Size(max = 50)
     @NotNull
     private String userName;
-
     @Size(max = 100)
     @NotNull
     private String email;
-
     @NotNull
     @Size(max = 2000)
     private String password;
-
     @Size(max = 100)
     private String firstName;
-
     @Size(max = 100)
     private String LastName;
-
     private String profilePicture;
-
     @NotNull
     private Role role;
 
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
+
 
 
